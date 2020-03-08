f311426f61e1c3c3dbf079a862f4d82ce96118f7
Vlad <50303876+tw9ntyseven@users.noreply.github.com>
2020-03-08 10:42:30 +0300

    bot in Telegram

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = ''
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message',msg =>{
+    bot.sendMessage(msg.chat.id, `"Привет, ${msg.from.first_name}"`)
+})
